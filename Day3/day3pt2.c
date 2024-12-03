40ae6812d3e9e838acfbb0f561c403e4fd9b6391
bry <greatmaharg@gmail.com>
2024-12-03 00:25:33 -0500

    Day 3 complete

---
+++ Day3/day3pt2.c
@@ -0,0 +1,46 @@
+#include "../utils.h"
+#include <ctype.h>
+#include <stdbool.h>
+
+int main(int argc, char** argv) {
+    FILE* f = fopen("input.txt", "r");
+    char* data = fread_dup(f);
+
+    char* dptr = data;
+    size_t score = 0;
+    bool enabled = true;
+    while(*dptr) {
+        if(strncmp(dptr, "do()", 4) == 0) {
+            dptr += 4;
+            enabled = true;
+        } else if(strncmp(dptr, "don't()", 7) == 0) {
+            dptr += 7;
+            enabled = false;
+        } else if(enabled && strncmp(dptr, "mul(", 4) == 0) {
+            dptr += 4;
+            if(!isdigit(*dptr)) continue;
+            int i = 0;
+            while(isdigit(*dptr)) {
+                i *= 10;
+                i += (*dptr) - '0';
+                dptr++;
+            }
+            if(*dptr != ',') continue;
+            dptr++;
+            if(!isdigit(*dptr)) continue;
+            int j = 0;
+            while(isdigit(*dptr)) {
+                j *= 10;
+                j += (*dptr) - '0';
+                dptr++;
+            }
+            if(*dptr != ')') continue;
+            score += i*j;
+        } else {
+            dptr++;
+        }
+    }
+    printf("%lu\n", score);
+
+    free(data);
+}
