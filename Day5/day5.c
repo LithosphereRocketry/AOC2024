1141f23cd804eead064297e2b816b5918c51eef8
bry <greatmaharg@gmail.com>
2024-12-05 01:20:31 -0500

    Completed day 5

---
+++ Day5/day5.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../utils.h"
+
+struct entry {
+    size_t len;
+    int* lst;
+};
+
+size_t ents_len = 0;
+struct entry* ents;
+
+bool entry_has(struct entry* entry, int value) {
+    for(size_t i = 0; i < entry->len; i++) {
+        if(entry->lst[i] == value) return true;
+    }
+    return false;
+}
+
+void entry_push(struct entry* entry, int value) {
+    if(entry_has(entry, value)) return;
+    entry->lst = realloc(entry->lst, (entry->len + 1)*sizeof(int));
+    entry->lst[entry->len] = value;
+    entry->len++;
+}
+
+void entry_merge(struct entry* into, const struct entry* from) {
+    for(size_t i = 0; i < from->len; i++) {
+        entry_push(into, from->lst[i]);
+    }
+}
+
+void entry_destroy(struct entry* entry) {
+    if(entry->lst) free(entry->lst);
+    entry->len = 0;
+    entry->lst = NULL;
+}
+
+void putent(int first, int second) {
+    if(second+1 >= ents_len) {
+        ents = realloc(ents, (second+1)*sizeof(struct entry));
+        for(size_t i = ents_len; i < second+1; i++) {
+            ents[i].len = 0;
+            ents[i].lst = NULL;
+        }
+        ents_len = second+1;
+    }
+    entry_push(&ents[second], first);
+}
+
+int main(int argc, char** argv) {
+    FILE* f = fopen("input.txt", "r");
+    int first, second;
+    while(fscanf(f, "%i|%i\n", &first, &second) == 2) {
+        putent(first, second);
+    }
+
+    int score = 0;
+    // first now contains the first number of the sequence, because scanf things
+    char* line;
+    while((line = fgets_dup(f)) && strlen(line) > 0) {
+        intList lnums;
+        il_init(&lnums);
+        char* lptr = line;
+        int inc;
+        if(first == -1) {
+            sscanf(lptr, "%i%n", &first, &inc);
+            lptr += inc;
+        }
+        il_append(&lnums, first);
+        while(sscanf(lptr, ",%i%n", &first, &inc) == 1) {
+            il_append(&lnums, first);
+            lptr += inc;
+        }
+
+        bool line_safe = true;
+        
+        struct entry ent = {0, NULL};
+        for(int i = 0; i < lnums.len; i++) {
+            int newnum = lnums.contents[i];
+            if(entry_has(&ent, newnum)) {
+                line_safe = false;
+                break;
+            }
+            if(newnum < ents_len) {
+                entry_merge(&ent, &ents[newnum]);
+            }
+        }
+        entry_destroy(&ent);
+        
+        if(!line_safe) {
+            for(int findpos = lnums.len; findpos > 0; findpos--) {
+                bool found = false;
+                for(int trypos = 0; trypos < findpos; trypos++) {
+                    int tryval = lnums.contents[trypos];
+                    for(int accpos = 0; accpos < findpos; accpos++) {
+                        int accval = lnums.contents[accpos];
+                        if(accpos != trypos && accval < ents_len) {
+                            entry_merge(&ent, &ents[accval]);
+                        }
+                    }
+                    if(!entry_has(&ent, tryval)) {
+                        lnums.contents[trypos] = lnums.contents[findpos-1];
+                        lnums.contents[findpos-1] = tryval;
+                        found = true;
+                        entry_destroy(&ent);
+                        break;
+                    }
+                    entry_destroy(&ent);
+                }
+                if(!found) {
+                    printf("FAIL!\n");
+                    exit(-1);
+                }
+            }
+            score += lnums.contents[lnums.len / 2];
+        }
+
+        first = -1;
+        il_destroy(&lnums);
+        free(line);
+    }
+    printf("%i\n", score);
+}
